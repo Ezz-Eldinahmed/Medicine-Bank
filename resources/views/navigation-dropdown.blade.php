2090b0ef0943f1f39479c9d4a3e5dca38483103f
Ezz-Eldin <49767083+Ezz-Eldinahmed@users.noreply.github.com>
2020-11-30 22:32:08 +0200

    make it better

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <img src="logo.png" width="150px" height="50px">  
                     </a>
                 </div>
 
